8e88b2fb67e32dc3f4cd52ddfd3855fb81736a4c
diego <diegoonate3026@gmail.com>
2023-06-03 10:32:40 -0500

    calculadora basica

---
+++ src/clases/CalculadoraGUI.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import grafica.ventanaCalculadora;
+
+/**
+ *
+ * @author asus
+ */
+public class CalculadoraGUI {
+    public static void main(String[] args) {
+        ventanaCalculadora calculadora = new ventanaCalculadora();
+        calculadora.setVisible(true);
+    }
+}
